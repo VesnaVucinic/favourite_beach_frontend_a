6005572d31bb3ee42c960e23a44de8e6c0a317a2
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-08-16 22:23:44 +0100

    add getBeaches function

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ function getBeaches() {
     fetch(endPoint)
     .then(response => response.json()) 
     .then(beaches => {
-        console.log(beaches)
+          // remember our JSON data is a bit nested due to our serializer
+        beaches.data.forEach(beach => {
+          // double check how your data is nested in the console so you can successfully access the attributes of each individual object
+          const beachMarkup = `
+            <div data-id=${beach.id}>
+              <h3>${beach.attributes.name}</h3>
+              <p>${beach.attributes.country.name}</p>
+              <p>${beach.attributes.location}</p>
+              <p>${beach.attributes.description}</p>
+              <img src=${beach.attributes.image_url} height="200" width="250">
+              <br><br>
+              <button data-id=${beach.id}>edit</button>
+            </div>
+            <br><br>`;
+  
+            document.querySelector('#beach-container').innerHTML += beachMarkup
+        })
     })
-}        
+}       
