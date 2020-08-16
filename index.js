33042b3f62ebe4d84f88dfe1e690f0b9af9778cd
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-08-16 22:11:43 +0100

    add event listener for DOM

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("in index")
+const endPoint = "http://127.0.0.1:3000/api/v1/beaches"
+
+document.addEventListener("DOMContentLoaded", () => {
+    getBeaches()
+})
+
+function getBeaches() {
+    fetch(endPoint)
+    .then(response => response.json()) 
+    .then(beaches => {
+        console.log(beaches)
+    })
+}        
