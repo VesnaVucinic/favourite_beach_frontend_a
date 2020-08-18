0a6e0cdb5682e6e067d9a37383bf16570a5f6d81
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-08-18 10:45:05 +0100

    add render function in postBeach

--- index.js
+++ index.js
@@ -14,25 +14,29 @@ function getBeaches() {
           // remember our JSON data is a bit nested due to our serializer
         beaches.data.forEach(beach => {
           // double check how your data is nested in the console so you can successfully access the attributes of each individual object
-          const beachMarkup = `
-            <div data-id=${beach.id}>
-              <h3>${beach.attributes.name}</h3>
-              <p>${beach.attributes.country.name}</p>
-              <p>${beach.attributes.location}</p>
-              <p>${beach.attributes.description}</p>
-              <img src=${beach.attributes.image_url} height="200" width="250">
-              <br><br>
-              <button data-id=${beach.id}>edit</button>
-            </div>
-            <br><br>`;
-  
-            document.querySelector('#beach-container').innerHTML += beachMarkup
+        render(beach)  
         })
     })
-}    
+}   
+
+function render(beach) {
+    const beachMarkup = `
+    <div data-id=${beach.id}>
+      <h3>${beach.attributes.name}</h3>
+      <p>${beach.attributes.country.name}</p>
+      <p>${beach.attributes.location}</p>
+      <p>${beach.attributes.description}</p>
+      <img src=${beach.attributes.image_url} height="200" width="250">
+      <br><br>
+      <button data-id=${beach.id}>edit</button>
+    </div>
+    <br><br>`;
+
+    document.querySelector('#beach-container').innerHTML += beachMarkup
+}
 
 function createFormHandler(event) {
-    event.preventDefault()   
+    event.preventDefault()
     const nameInput = document.querySelector('#input-name').value
     const countryId = parseInt(document.querySelector('#countries').value)
     const locationInput = document.querySelector('#input-location').value
@@ -42,4 +46,22 @@ function createFormHandler(event) {
     postBeach(nameInput, countryId, locationInput, descriptionInput, imageInput)
 }
 
+function postBeach(name, country_id, location, description, image_url) {
+    // console.log(name, country_id, location, description, image_url);
+    let bodyData = {name, country_id, location, description, image_url}
 
+    fetch(endPoint, {
+        // POST request
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(bodyData)
+    })
+    .then(response => response.json())
+    .then(beach => {
+        console.log(beach)
+        
+        const beachData =  beach.data
+        // render JSON response, render data to user to see what created, manuplate DOM by showing user what created, data is pointing to single object not array like in get fetch where I had arrey and .forEach
+        render(beachData)  
+    })
+}
