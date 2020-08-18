59b3d10c88d1baea1c9500da43039a6e37645ee3
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-08-18 19:01:41 +0100

    add beach class with constructor and render beachCard

--- src/beach.js
+++ src/beach.js
@@ -1,3 +1,35 @@
+
 class Beach {
+    // map attributes from backend (take values from db) and map those on to beach instances
+    constructor(beach, beachAttributes) {
+        this.id = beach.id
+        // debugger
+        this.name = beachAttributes.name
+        this.country = beachAttributes.country
+        this.location = beachAttributes.location
+        this.description = beachAttributes.description
+        this.image_url = beachAttributes.image_url
+        Beach.all.push(this)
+        // debugger
+    }
+
+    renderBeachCard() {
+        // debugger
+        return `
+        <div data-id=${this.id}>
+          <h3>${this.name}</h3>
+          <p>${this.country.name}</p>
+          <p>${this.location}</p>
+          <p>${this.description}</p>
+          <img src=${this.image_url} height="200" width="250">
+          <br><br>
+          <button data-id=${this.id}>edit</button>
+        </div>
+        <br><br>`;
     
+    }
 }
+
+Beach.all = [];
+
+
