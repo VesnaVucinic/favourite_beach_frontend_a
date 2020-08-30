c902bedfaaf0ff2ef06d318bd065a5b5c8dab0a8
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-08-30 17:29:37 +0100

    add renderUpdateForm

--- src/beach.js
+++ src/beach.js
@@ -57,6 +57,61 @@ class Beach {
     static findById(id) {
       return this.all.find(beach => beach.id === id);
     }
+
+    renderUpdateForm() {
+      return `
+      <section class="jumbotron text-center">
+      <div class="container">
+        <form data-id=${this.id}>
+          <h3>Edit a Beach!</h3>
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="inputGroup-sizing-default">Name</span>
+            </div>
+            <input id='input-name' type="text" value="${this.name}" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+          </div> 
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="inputGroup-sizing-default">Location</span>
+            </div>
+            <input id='input-location' type="text" value="${this.location}" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+          </div>
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text">Description</span>
+            </div>
+            <textarea id='input-description' type="text" value="${this.description}" class="form-control" aria-label="With textarea"></textarea>
+          </div>
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="basic-addon3">Image Url</span>
+            </div>
+            <input id='input-url' value="${this.image_url}" type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+          </div>
+  
+          <div class="input-group mb-3">
+              <div class="input-group-prepend">
+                <label class="input-group-text" for="inputGroupSelect01">Select a Country</label>
+              </div>
+              <select id="countries" value="${this.country.name}" class="custom-select" id="inputGroupSelect01">
+                <option selected></option>
+                <option value="1">Greece</option>
+                <option value="2">Spain</option>
+                <option value="3">France</option>
+                <option value="4">Portugal</option>
+                <option value="5">Cyprus</option>
+                <option value="6">Italy</option>
+                <option value="7">Croatia</option>
+                <option value="8">Great Britain</option>
+              </select>
+            </div>
+          <button id="edit-button" type="submit" class="btn btn-primary">Edit Beach</button>
+          <br><br>
+      </form>
+      </div>
+    </section>
+    `
+ }
 }
 
 Beach.all = [];
