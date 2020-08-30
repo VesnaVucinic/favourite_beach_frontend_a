ed88796e2a0a2c0b42dc50988a8556556008e414
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-08-30 20:46:23 +0100

    add fixed renderBeachCard

--- src/beach.js
+++ src/beach.js
@@ -1,62 +1,59 @@
 
 class Beach {
-    // map attributes from backend (take values from db) and map those on to beach instances
-    constructor(beach, beachAttributes) {
-        this.id = beach.id
-        // debugger
-        this.name = beachAttributes.name
-        this.country = beachAttributes.country
-        this.location = beachAttributes.location
-        this.description = beachAttributes.description
-        this.image_url = beachAttributes.image_url
-        Beach.all.push(this)
-        console.log(this)
-        // debugger
-    }
+  // map attributes from backend (take values from db) and map those on to beach instances
+  constructor(beach, beachAttributes) {
+      this.id = beach.id
+      // debugger
+      this.name = beachAttributes.name
+      this.country = beachAttributes.country
+      this.location = beachAttributes.location
+      this.description = beachAttributes.description
+      this.image_url = beachAttributes.image_url
+      Beach.all.push(this)
+      // console.log(this)
+      // debugger
+  }
 
-    renderBeachCard() {
-        // debugger
-        // return `
-        // <div data-id=${this.id}>
-        //   <h3>${this.name}</h3>
-        //   <p>${this.country.name}</p>
-        //   <p>${this.location}</p>
-        //   <p>${this.description}</p>
-        //   <img src=${this.image_url} height="200" width="250">
-        //   <br><br>
-        //   <button data-id=${this.id}>edit</button>
-        // </div>
-        // <br><br>`;
+  renderBeachCard() {
+      // debugger
+      // return `
+      // <div data-id=${this.id}>
+      //   <h3>${this.name}</h3>
+      //   <p>${this.country.name}</p>
+      //   <p>${this.location}</p>
+      //   <p>${this.description}</p>
+      //   <img src=${this.image_url} height="200" width="250">
+      //   <br><br>
+      //   <button data-id=${this.id}>edit</button>
+      // </div>
+      // <br><br>`;
 
-        return `
-        <div class="col-md-3">
-          <div class="card mb-4 shadow-sm">
-            <img src=${this.image_url} class="card-img-top" alt="...">
-            <div class="card text-center" >
-
-            <div class="card-body">
-              <h6 class="card-title">${this.name}</h6>
-              <p class="card-title">Location: ${this.location}</p>
-              <p class="card-title">Country: ${this.country.name}</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+  return `
+      <div class="col-md-4">
+        <div class="card mb-4 shadow-sm">
+          <img src=${this.image_url} class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${this.name}</h5>
+            <h5 class="card-title">${this.location}</h5>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <button type="button" data-id=${this.id} class="btn btn-sm btn-outline-secondary">View</button>
+                <button type="button" data-id=${this.id} class="btn btn-sm btn-outline-secondary">Edit</button>
               </div>
-              </div>
+              <small class="text-muted">Country: ${this.country.name}</small>
             </div>
           </div>
         </div>
+      </div>
+      <br><br>
+    `
+  }
+  // <p class="card-text">${this.description}</p>
 
-        
-        <br><br>
-      `
-      // <p class="card-text">${this.description}</p>
-
-    }
-
-    static findById(id) {
-      return this.all.find(beach => beach.id === id);
-    }
+ 
+  static findById(id) {
+    return this.all.find(beach => beach.id === id);
+  }
 }
 
 Beach.all = [];
