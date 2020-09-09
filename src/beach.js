b540024c6f223ff0a2d195a9a3ca9098fdb61745
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-09-09 18:14:14 +0100

    add delete action

--- src/beach.js
+++ src/beach.js
@@ -29,7 +29,7 @@ class Beach {
       // <br><br>`;
 
   return `
-      <div class="col-md-4">
+      <div  class="col-md-4">
         <div class="card mb-4 shadow-sm">
           <img src=${this.image_url} class="card-img-top" alt="...">
           <div class="card text-center" >
@@ -40,7 +40,8 @@ class Beach {
             <p class="card-title">Country: ${this.country.name}</p>
 
             <div class="d-flex justify-content-between align-items-center">
-                <button type="button" data-id=${this.id} class="btn btn-sm btn-outline-secondary">View</button>
+            <button type="submit" onClick="deleteBeach();" data-id="${this.id}" class="delete">Delete</button>
+
             </div>
           </div>
         </div>
@@ -48,87 +49,88 @@ class Beach {
       <br><br>
     `
   }
-  // <p class="card-text">${this.description}</p>
+  // <p class="card-text">${this.description}</p>                 <button type="button" data-id=${this.id} class="btn btn-sm btn-outline-secondary">View</button>
 
-  static findById(id) {
-    return this.all.find(beach => beach.id === id);
-  }
 
-  renderShowBeach() {
-    return `
-    <div class="col-md-12">
-      <div class="card mb-4 shadow-sm">
-        <img src=${this.image_url} class="card-img-top" alt="...">
-        <div class="card text-center" >
+  // static findById(id) {
+  //   return this.all.find(beach => beach.id === id);
+  // }
+
+  // renderShowBeach() {
+  //   return `
+  //   <div data-id="${this.id}" class="card" class="col-md-12">
+  //     <div class="card mb-4 shadow-sm">
+  //       <img src=${this.image_url} class="card-img-top" alt="...">
+  //       <div class="card text-center" >
   
-        <div class="card-body">
-          <h6 class="card-title">${this.name}</h6>
-          <p class="card-title">Location: ${this.location}</p>
-          <p class="card-title">Country: ${this.country.name}</p>
-          <p class="card-text">${this.description}</p>
-          <div class="d-flex justify-content-between align-items-center">
-              <button type="button" data-id=${this.id} class="btn btn-sm btn-outline-secondary">Edit</button>
-          </div>
-        </div>
-      </div>
-    </div>
-    <br><br>
-  `
-  }
+  //       <div class="card-body">
+  //         <h6 class="card-title">${this.name}</h6>
+  //         <p class="card-title">Location: ${this.location}</p>
+  //         <p class="card-title">Country: ${this.country.name}</p>
+  //         <p class="card-text">${this.description}</p>
+  //         <div class="d-flex justify-content-between align-items-center">
+  //             <button type="button" data-id=${this.id} class="btn btn-sm btn-outline-secondary">Delete</button>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </div>
+  //   <br><br>
+  // `
+  // }
 
-  renderUpdateForm() {
-    return `
-    <section class="jumbotron text-center">
-    <div class="container">
-      <form data-id=${this.id}>
-        <div class="input-group mb-3">
-          <div class="input-group-prepend">
-            <span class="input-group-text" id="inputGroup-sizing-default">Name</span>
-          </div>
-          <input id='input-name' type="text" value="${this.name}" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
-        </div> 
-        <div class="input-group mb-3">
-          <div class="input-group-prepend">
-            <span class="input-group-text" id="inputGroup-sizing-default">Location</span>
-          </div>
-          <input id='input-location' type="text" value="${this.location}" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
-        </div>
-        <div class="input-group mb-3">
-          <div class="input-group-prepend">
-            <span class="input-group-text">Description</span>
-          </div>
-          <input id='input-description' type="text" value="${this.description}" class="form-control">
-        </div>
-        <div class="input-group mb-3">
-          <div class="input-group-prepend">
-            <span class="input-group-text" id="basic-addon3">Image Url</span>
-          </div>
-          <input id='input-url' value="${this.image_url}" type="text" class="form-control"  aria-describedby="basic-addon3">
-        </div>
+  // renderUpdateForm() {
+  //   return `
+  //   <section class="jumbotron text-center">
+  //   <div class="container">
+  //     <form data-id=${this.id}>
+  //       <div class="input-group mb-3">
+  //         <div class="input-group-prepend">
+  //           <span class="input-group-text" id="inputGroup-sizing-default">Name</span>
+  //         </div>
+  //         <input id='input-name' type="text" value="${this.name}" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+  //       </div> 
+  //       <div class="input-group mb-3">
+  //         <div class="input-group-prepend">
+  //           <span class="input-group-text" id="inputGroup-sizing-default">Location</span>
+  //         </div>
+  //         <input id='input-location' type="text" value="${this.location}" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+  //       </div>
+  //       <div class="input-group mb-3">
+  //         <div class="input-group-prepend">
+  //           <span class="input-group-text">Description</span>
+  //         </div>
+  //         <input id='input-description' type="text" value="${this.description}" class="form-control">
+  //       </div>
+  //       <div class="input-group mb-3">
+  //         <div class="input-group-prepend">
+  //           <span class="input-group-text" id="basic-addon3">Image Url</span>
+  //         </div>
+  //         <input id='input-url' value="${this.image_url}" type="text" class="form-control"  aria-describedby="basic-addon3">
+  //       </div>
 
-        <div class="input-group mb-3">
-            <div class="input-group-prepend">
-              <label class="input-group-text" for="inputGroupSelect01">Select a Country</label>
-            </div>
-            <select id="countries" value="${this.country.name}" class="custom-select" id="inputGroupSelect01">
-              <option selected></option>
-              <option value="1">Greece</option>
-              <option value="2">Spain</option>
-              <option value="3">France</option>
-              <option value="4">Portugal</option>
-              <option value="5">Cyprus</option>
-              <option value="6">Italy</option>
-              <option value="7">Croatia</option>
-              <option value="8">Great Britain</option>
-            </select>
-          </div>
-        <button id="edit-button" type="submit" class="btn btn-primary" class ="submit">Edit Beach</button>
-        <br><br>
-    </form>
-    </div>
-  </section>
-  `
-  }
+  //       <div class="input-group mb-3">
+  //           <div class="input-group-prepend">
+  //             <label class="input-group-text" for="inputGroupSelect01">Select a Country</label>
+  //           </div>
+  //           <select id="countries" value="${this.country.name}" class="custom-select" id="inputGroupSelect01">
+  //             <option selected></option>
+  //             <option value="1">Greece</option>
+  //             <option value="2">Spain</option>
+  //             <option value="3">France</option>
+  //             <option value="4">Portugal</option>
+  //             <option value="5">Cyprus</option>
+  //             <option value="6">Italy</option>
+  //             <option value="7">Croatia</option>
+  //             <option value="8">Great Britain</option>
+  //           </select>
+  //         </div>
+  //       <button id="edit-button" type="submit" class="btn btn-primary" class ="submit">Edit Beach</button>
+  //       <br><br>
+  //   </form>
+  //   </div>
+  // </section>
+  // `
+  // }
 }
 
 Beach.all = [];
--- src/index.js
+++ src/index.js
@@ -12,32 +12,47 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // listen for 'click' event on beach container
     
-    let beachContainer = document.querySelector('#beach-container')
-    beachContainer.addEventListener('click', event => {
-        // console.log('clicked');
-        let id = event.target.dataset.id;
-        // debugger
-        let beach = Beach.findById(id);
-        // debugger
-        // console.log(beach);
-        document.querySelector('#show-beach').innerHTML = beach.renderShowBeach();   
-    }) 
+    // let beachContainer = document.querySelector('#beach-container')
+    // beachContainer.addEventListener('click', event => {
+    //     // console.log('clicked');
+    //     let id = event.target.dataset.id;
+    //     // debugger
+    //     let beach = Beach.findById(id);
+    //     // debugger
+    //     // console.log(beach);
+    //     document.querySelector('#show-beach').innerHTML = beach.renderShowBeach();   
+    // }) 
 
-    let showBeachContainer = document.querySelector('#show-beach')
-    showBeachContainer.addEventListener('click', event => {
-        // console.log('clicked');
-        let id = event.target.dataset.id;
-        // debugger
-        let beach = Beach.findById(id);
-        // debugger
-        console.log(beach);
-        // Render the edit form when the edit button is clicked
-        // debugger
-        document.querySelector('#update-beach').innerHTML = beach.renderUpdateForm();
-        // listen for the submit event of the edit form and handle the data
-        document.querySelector('#update-beach').addEventListener('submit', event => updateFormHandler(event)) 
-    })
-})  
+    // let showBeachContainer = document.querySelector('#show-beach')
+    // showBeachContainer.addEventListener('click', event => {
+    //     // console.log('clicked');
+    //     let id = event.target.dataset.id;
+    //     // debugger
+    //     let beach = Beach.findById(id);
+    //     // debugger
+    //     console.log(beach);
+    //     // Render the edit form when the edit button is clicked
+    //     // debugger
+    //     document.querySelector('#update-beach').innerHTML = beach.renderUpdateForm();
+    //     // listen for the submit event of the edit form and handle the data
+    //     document.querySelector('#update-beach').addEventListener('submit', event => updateFormHandler(event)) 
+    // })
+
+    // let showBeachContainer = document.querySelector('#show-beach')
+    // showBeachContainer.addEventListener('click', event => {
+    //     // console.log('clicked');
+    //     let id = event.target.dataset.id;
+    //     // debugger
+    //     let beach = Beach.findById(id);
+    //     // debugger
+    //     console.log(beach);
+    //     deleteBeach()
+    // })
+
+    // const deleteButton = document.querySelector(".delete");
+    // deleteButton.addEventListener("click", (event) => deleteBeach());
+});
+  
 
 function getBeaches() {
     fetch(endPoint)
@@ -143,37 +158,94 @@ function postBeach(name, country_id, location, description, image_url) {
     })
 }
 
-function updateFormHandler(event) {
-    event.preventDefault();
-    let id = event.target.dataset.id;
-    let beach = Beach.findById(id);
-    // debugger
-    const name = event.target.querySelector('#input-name').value;
-    const location = event.target.querySelector('#input-location').value;
-    const description = event.target.querySelector('#input-description').value;
-    const image_url = event.target.querySelector('#input-url').value;
-    const country_id = parseInt(event.target.querySelector('#countries').value);
-    patchBeach(beach, name, location, description, image_url, country_id)
-    eraseText();
-}
+// function updateFormHandler(event) {
+//     event.preventDefault();
+//     let id = event.target.dataset.id;
+//     let beach = Beach.findById(id);
+//     // debugger
+//     const name = event.target.querySelector('#input-name').value;
+//     const location = event.target.querySelector('#input-location').value;
+//     const description = event.target.querySelector('#input-description').value;
+//     const image_url = event.target.querySelector('#input-url').value;
+//     const country_id = parseInt(event.target.querySelector('#countries').value);
+//     patchBeach(beach, name, location, description, image_url, country_id)
+//     eraseText();
+// }
 
- // Send the PATCH Request to the Backend
-function patchBeach(beach, name, location, description, image_url, country_id) {
-    // debugger
-    const bodyJSON = { beach, name, location, description, image_url, country_id }
-    fetch(`http://localhost:3000/api/v1/beaches/${beach.id}`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-        "Authorization": `Bearer ${localStorage.getItem('jwt_token')}`
-      },
-      body: JSON.stringify(bodyJSON),
-    })
-      .then(res => res.json())
-      // our backend responds with the updated beach instance represented as JSON
+//  // Send the PATCH Request to the Backend
+// function patchBeach(beach, name, location, description, image_url, country_id) {
+//     // debugger
+//     const bodyJSON = { beach, name, location, description, image_url, country_id }
+//     fetch(`http://localhost:3000/api/v1/beaches/${beach.id}`, {
+//       method: 'PATCH',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         "Authorization": `Bearer ${localStorage.getItem('jwt_token')}`
+//       },
+//       body: JSON.stringify(bodyJSON),
+//     })
+//       .then(res => res.json())
+//       // our backend responds with the updated beach instance represented as JSON
       
-      .then(updatedBeach => console.log(updatedBeach));
+//       .then(updatedBeach => console.log(updatedBeach));
+//     //   debugger
+// };
+
+// const deleteBeach = (event) => {
+//     event.preventDefault()
+//     const configObj = {
+//         method: "DELETE",
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Accept": "application/json",
+//             "Authorization": `Bearer ${localStorage.getItem('jwt_token')}`
+//         }
+//     }
+
+//     fetch(`${endPoint}/${event.target.dataset.beachId}`, configObj)
+//     event.target.parentElement.remove()
+// }
+
+// function deleteBeach (event) {
+//     debugger
+//     event.preventDefault()
+//     const configObj = {
+//         method: "DELETE",
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Accept": "application/json",
+//             "Authorization": `Bearer ${localStorage.getItem('jwt_token')}`
+//         }
+//     }
+
+//     fetch(`${endPoint}/${event.target.dataset.beachId}`, configObj)
+//     event.target.parentElement.remove()
+// }
+
+
+function deleteBeach() {
+    let beachDataId = event.target.dataset.id;
+    // debugger
+    fetch(`http://127.0.0.1:3000/api/v1/beaches/${beachDataId}`, {
+      method: "DELETE",
+      headers: {
+                
+                "Authorization": `Bearer ${localStorage.getItem('jwt_token')}`
+              },
+    })
+    
+    // event.target.parentNode.removeChild()
+    //   .then((response) => response.json())
+    //   .then((json) => {s
+    // debugger
+        // let selectedBeach = document.querySelector(
+        //   `.card[data-id="${beachDataId}"]`
+        // );
+        // // debugger
+        // selectedBeach.remove();
+        event.target.parentElement.remove()
+    //   });
     //   debugger
-};
+}
 
 
